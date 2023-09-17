5e00d2e764d4b465fb94c8f5211828ab3091b2f8
Dinesh <dineshbellam98@gmail.com>
2023-09-17 17:52:27 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Header from "./components/Header";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Cart from "./components/Cart";
 
@@ -8,14 +8,10 @@ function App() {
   return (
     <BrowserRouter>
       <Header />
-      <div className="App">
-        <Route path="/" exact>
-          <Home />
-        </Route>
-        <Route path="/cart">
-          <Cart />
-        </Route>
-      </div>
+      <Routes className="App">
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/cart" element={<Cart />}></Route>
+      </Routes>
     </BrowserRouter>
   );
 }
